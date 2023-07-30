9c8f05dcd2aea68949dc10a8dfb97f26cccc4025
Tatiana Osipova <otv120780@gmail.com>
2023-07-30 15:45:28 -0400

    feat: add slider

--- js/main.js
+++ js/main.js
@@ -4,3 +4,15 @@ const menuMobile = document.querySelector('.header__menu-list');
 menuBtn.addEventListener('click', () => {
     menuMobile.classList.toggle('menu--open')
 });
+
+const swiperOne = new Swiper('.feedback__slider', {
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+  
+    pagination: {
+      el: '.swiper-pagination',
+    },      
+});
